56d7faae491226e9fa14e9c4973a5b1676fb5803
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-22 13:30:30 +0530

    Update MusicDataVerify.java

--- src/test/java/com/infosys/ea/musicdata/MusicDataVerify.java
+++ src/test/java/com/infosys/ea/musicdata/MusicDataVerify.java
@@ -1,3 +1,6 @@
+/*Note : This class is extended by using the Base class. launching browser, open & close browser will be handled by base class.*/
+
+
 package com.infosys.ea.musicdata;
 
 import org.testng.Assert;
@@ -6,10 +9,13 @@ import org.testng.annotations.Test;
 import com.infosys.ea.testbase.BaseClass;
 
 public class MusicDataVerify extends BaseClass{
+	// verifying the webpage title
   @Test
-  public void verifyPageTitle() {
+  public void verifyPageTitle() 
+  {
 	  String expectedtitle="EaCodingTests";
 	  String actualtitle=driver.getTitle();
+ // comparing the expected and actual title using assertions
 	  Assert.assertEquals(actualtitle, expectedtitle);
   }
 }
