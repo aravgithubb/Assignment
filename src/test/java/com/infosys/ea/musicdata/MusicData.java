1412624902fc982cc26d6bc9ec66eed847c4dca6
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-22 13:24:56 +0530

    Update MusicData.java

--- src/test/java/com/infosys/ea/musicdata/MusicData.java
+++ src/test/java/com/infosys/ea/musicdata/MusicData.java
@@ -1,3 +1,6 @@
+
+/*Note : This class is extended by using the Base class. launching browser, open & close browser will be handled by base class.*/
+	
 package com.infosys.ea.musicdata;
 
 import java.util.List;
@@ -9,13 +12,21 @@ import org.testng.annotations.Test;
 import com.infosys.ea.testbase.BaseClass;
 
 public class MusicData extends BaseClass {
+	
 	@Test
-	public void getAllData() throws Throwable {
-	    List<WebElement> elements = driver.findElements(By.xpath("/html/body/app-root/app-festivals/ol/li"));
-	//  List<WebElement> elements = driver.findElements(By.xpath("//ol[@style='user-select: auto;']/li"));
+	//getting the text of webelement and printing in the console
+	public void getAllData() throws Throwable 
+	{
+		// finding multiple elements and holding in the list
+	        List<WebElement> elements = driver.findElements(By.xpath("/html/body/app-root/app-festivals/ol/li"));
+		// getting and printing the found elements number in console.
 		System.out.println("no of elements" + elements.size());
-		for (WebElement elem : elements) {
+		// looping the elements to get the individual element details.
+		for (WebElement elem : elements) 
+		{
+			// printing the elements text in the console.
 			System.out.println("Element " + elem.getText());
+			
 		}
 	}
 }
