2de38360b2cf2816c0e64471839d74ea2d944067
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-22 19:19:37 +0530

    Update MusicGetSele.java

--- src/test/java/com/infosys/ea/musicdata/MusicGetSele.java
+++ src/test/java/com/infosys/ea/musicdata/MusicGetSele.java
@@ -15,17 +15,21 @@ import com.infosys.ea.testbase.BaseClass;
 public class MusicGetSele extends BaseClass {
 	@Test
 	public void getSelected() throws Throwable {
+		// declaring the string 
 		String expected="Frank Jupiter";
+		// finding elements
 		List<WebElement> elements = driver.findElements(By.xpath("/html/body/app-root/app-festivals/ol/li"));
+		// printing the number of elements found
 		System.out.println("no of elements" + elements.size());
 		Thread.sleep(5000);
+		 using loop to extract the elements and perform operation.
 		for (WebElement elem : elements) {
 			if (elem.getText().equals("Propeller"))
 
 			{
-				// verifying the getting text with expected
-		                SoftAssert verify= new SoftAssert();
-				verify.assertEquals(elem, expected);
+				// verifying the getting text with expected and printing the text of webelement.
+		                System.out.println("Element " + elem.getText());
+			         Assert.assertEquals(elem, expected);;
 			}
 		}
 	}
