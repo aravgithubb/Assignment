9a19fa98dabf3cf434d2cf77e2f75fa66c74e49e
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-22 13:12:29 +0530

    Update BaseClass.java

--- src/test/java/com/infosys/ea/testbase/BaseClass.java
+++ src/test/java/com/infosys/ea/testbase/BaseClass.java
@@ -1,3 +1,9 @@
+
+/*Note :This BaseClass deals with  the common functions used by all the pages like open browser, close browser, Login, Logout, waiting mechanism, launch URL.
+        This class will be extended to the test cases required.*/
+      
+
+
 package com.infosys.ea.testbase;
 
 import java.time.Duration;
@@ -15,23 +21,26 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 
 	public WebDriver driver;
-
+        // open browser launch url and maximize
 	@BeforeClass
 	public void openBrowser() {
+		// launching the browser
 		WebDriverManager.chromedriver().setup();
-		// System.setProperty("webdriver.chrome.driver",".//drivers/chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		DesiredCapabilities cp = new DesiredCapabilities();
 		cp.setCapability(ChromeOptions.CAPABILITY, options);
 		options.merge(cp);
-	    driver = new ChromeDriver(options);
+	        driver = new ChromeDriver(options);
+		// launching the url
 		driver.navigate().to("http://localhost:4200/festivals");
+		// waiting mechanism to overcome the load issues.
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		//maximize the browser
 		driver.manage().window().maximize();
 
 	}
-
+        // Logged into the Application
 	@BeforeMethod
 	public void login()
 
@@ -39,13 +48,13 @@ public class BaseClass {
 		System.out.println("Logged In");
 
 	}
-
+	// Logged out of the Application
 	@AfterMethod
 	public void logout() {
 		System.out.println("Log out");
 
 	}
-
+        // browser closed
 	@AfterClass
 	public void closeBrowser() {
 		driver.close();
